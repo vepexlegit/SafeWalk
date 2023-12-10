10cd5204e5f78342cf0c42741a1e2462b4cb851e
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-12-10 19:07:46 +0400

    Create MixinLoader.java

---
+++ src/main/java/de/vepexlegit/safewalkdebug/mixin/MixinLoader.java
@@ -0,0 +1,36 @@
+package de.vepexlegit.safewalkdebug.mixin;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+import java.util.Map;
+
+public class MixinLoader implements IFMLLoadingPlugin {
+    public MixinLoader() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.safewalkdebug.json");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(final Map<String, Object> data) {}
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
